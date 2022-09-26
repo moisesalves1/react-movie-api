2714fe6fff6eefcda719374c2ca512b293e4b401
moisesalves1 <moisesalves1@gmail.com>
2022-09-26 13:38:31 -0300

    Instalado Styled-Components e pagina Home otimizada para dinamico

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,6 +1,54 @@
+import { Container, MovieList, Movie } from "./styles";
+
 function Home(){
+
+    const movies = [
+        {
+            id: 1,
+            title: 'Spider Man',
+            image_url: 'https://upload.wikimedia.org/wikipedia/pt/0/00/Spider-Man_No_Way_Home_poster.jpg'
+        },
+        {
+            id: 2,
+            title: 'Batman',
+            image_url: 'https://upload.wikimedia.org/wikipedia/pt/0/00/Spider-Man_No_Way_Home_poster.jpg'
+        },
+        {
+            id: 3,
+            title: 'Avengers',
+            image_url: 'https://upload.wikimedia.org/wikipedia/pt/0/00/Spider-Man_No_Way_Home_poster.jpg'
+        },
+        {
+            id: 1,
+            title: 'Spider Man',
+            image_url: 'https://upload.wikimedia.org/wikipedia/pt/0/00/Spider-Man_No_Way_Home_poster.jpg'
+        },
+        {
+            id: 2,
+            title: 'Batman',
+            image_url: 'https://upload.wikimedia.org/wikipedia/pt/0/00/Spider-Man_No_Way_Home_poster.jpg'
+        },
+        {
+            id: 3,
+            title: 'Avengers',
+            image_url: 'https://upload.wikimedia.org/wikipedia/pt/0/00/Spider-Man_No_Way_Home_poster.jpg'
+        }
+    ]
+
     return(
-        <h1>Hello World!</h1>
+        <Container>
+            <h1>Movies</h1>
+            <MovieList>
+                {movies.map(movie => {
+                    return (
+                        <Movie key={movie.id}>
+                            <a href="https://google.com"><img src={movie.image_url} alt={movie.title}/></a>
+                            <span>{movie.title}</span>
+                        </Movie>
+                    )
+                })}
+            </MovieList>
+        </Container>
     )
 }
 
